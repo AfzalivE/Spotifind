3cb49e37da9ae1c4d5e4b20aaaaa40fd8c9f8677
Afzal Najam <afzalnaj@gmail.com>
2016-12-11 21:05:51 -0500

    Renamed backend Tunefind class

--- backend/src/Tunefind.js
+++ backend/src/Tunefind.js
@@ -3,7 +3,7 @@ import { Client } from 'tunefind'
 const TUNEFIND_USERNAME = '74d7e40fbd7c452f860ff0cd41633d90'
 const TUNEFIND_PASSWORD = '23f359a9fd7dbcdac039fc433f54ad18'
 
-class TunefindRepository extends Client {
+class Tunefind extends Client {
 
   constructor () {
     super({
@@ -15,5 +15,5 @@ class TunefindRepository extends Client {
   }
 }
 
-export default new TunefindRepository() // this instance will be shared across imports
+export default new Tunefind() // this instance will be shared across imports
 
