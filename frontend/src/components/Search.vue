13a45cec4361bc1a0d2413fc369a0804302703f2
Afzal Najam <afzalnaj@gmail.com>
2016-12-29 12:00:28 +0300

    Added more search page items

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>Hi</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
+        <div class="card card-signup">
+          <form class="form">
+            <div class="header header-primary text-center">
+              <h4>Search</h4>
+            </div>
+            <div class="content">
+              <div class="input-group">
+                <div class="form-group is-empty">
+                  <input type="text" class="form-control" placeholder="Search">
+                  <span class="material-input"></span>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -15,4 +35,7 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.card-signup {
+  margin: 160px 0 40px;
+}
 </style>
